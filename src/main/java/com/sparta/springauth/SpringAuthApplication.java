7e0d7f59a54c5c348efd488777c860722ef424fe
seungtaemoon <Robbie@email.com>
2023-08-31 14:52:03 +0900

    Init commit

--- src/main/java/com/sparta/springauth/SpringAuthApplication.java
+++ src/main/java/com/sparta/springauth/SpringAuthApplication.java
@@ -2,8 +2,10 @@ package com.sparta.springauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+// 기술지원 Bean 수동 등록
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // Spring Security 인증 기능 제외
 public class SpringAuthApplication {
 
     public static void main(String[] args) {
